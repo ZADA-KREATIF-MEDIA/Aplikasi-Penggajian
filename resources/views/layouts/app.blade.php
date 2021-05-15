629a3bd01bd179b9ee76b34c73fa728eeaba04f4
Marcelino15 <tegar.marcelino@gmail.com>
2021-05-15 20:56:22 +0700

    update 15-05-2021 by marcelino

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,11 +7,11 @@
     <title>@yield('title')</title>
 
     <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="{{url('adminlte/css/fontgoogle.css')}}">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="{{url('adminlte/plugins/fontawesome-free/css/all.min.css')}}">
     <!-- Ionicons -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="{{url('adminlte/css/ionicons.min.css')}}">
     <!-- Tempusdominus Bootstrap 4 -->
     <link rel="stylesheet" href="{{url('adminlte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
     <!-- iCheck -->
